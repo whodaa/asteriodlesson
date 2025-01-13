18dd4e3b76fedf79b37f6d7244297f770b09bd8e
Whodaa <andyeisen@hotmail.com>
2025-01-12 16:57:11 -0800

    game loop

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    frameclock = pygame.time.Clock() 
+
 
     while run_game_loop:
         for event in pygame.event.get():
@@ -20,6 +23,14 @@ def main():
         
         
         pygame.display.flip()
+        dt = safe_divide(frameclock.tick(60), 1000)
+
+def safe_divide(dividend, divisor):
+    if dividend == 0 or divisor == 0:
+        return 0
+    else:
+        return dividend / divisor
+
 
 if __name__ == "__main__":
     main()
